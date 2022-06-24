6bea8683b3dd62d7fab2499ff9d2a539568cc063
hasibulnasim <106619399+hasibulnasim@users.noreply.github.com>
2022-06-24 18:01:29 +0600

    Add files via upload
    Initial Commit- Lab Task 1 uploaded.

---
+++ lab1.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title>Resume</title>
+</head>
+<body>
+<table border="1" align="center" width="65%">
+<tr>
+<th> <h1>Welcome To My Resumes!</h1></th></tr>
+<tr> <th align="left"> <h2>Hasibul Nasim</h2>
+<b>Software Developer</b><br><br>
+(143/1 Alep Khan Road, Sharulia Bazar)<br><br>
+Demra, Dhaka-1360<br><br>
+Phone :01568758269<br><br>
+Email Address:hasibulnasim387@gmail.com<br><br>
+<h2>Personal Information</h2>
+ 
+   Father name:Abdul Haque<br><br>
+   Mother name:Nurun Nahar<br><br>
+   Date Of Birth:03/8/1998<br><br>
+   Religion:Islam<br><br>
+   Nationality:Bangladeshi<br><br>
+   Marital Status:Unmarried<br><br>
+
+<h2>Key Skills</h2>
+ <u1><li>C#</li>
+     <li>My SQL</li>
+     <li>JAVA</li>
+     <li>Oracle</li>
+     <li>C</li>
+
+<h2>Personal Abilities</h2>
+ <u1><li>Possesses great analytical and problem solving skills.</li>
+     <li>Able to work harmoniously with other member of the teams.</li>
+ </u1>
+
+
+<h2>Academic Qualification</h2>
+<table border="1"> <tr> <th>Educational Institution</th><th> Examination </th> <th> Grade</th></tr>
+<tr><td> Bir Shrestha Noor Mohammed Public College</td> <td>SSC</td> <td>5.00</td></tr>
+<tr><td> Bir Shrestha Noor Mohammed Public College</td> <td>HSC</td> <td>4.46</td></tr>
+<tr><td> American International University-Bangladesh</td> <td>Bsc.CSE</td> <td>3.03</td></tr>
+</table>
+
+</body>
+</html>
